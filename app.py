3e8e01002d5d77d5790c7130353f6ac3889e8e63
alexm323 <alejandromartinezjr42@gmail.com>
2020-09-01 15:26:36 -0700

    Step Zero for Cupcake API with setup

---
+++ app.py
@@ -0,0 +1,16 @@
+"""Flask app for Cupcakes"""
+from flask import Flask, request, render_template,  redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+# from models import db,  connect_db, Model_Name_Sample
+# from forms import Sample_Form
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "ILessThan3You"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app
